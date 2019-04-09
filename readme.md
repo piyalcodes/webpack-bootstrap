d4cb65c824bf1ed95be4ee14f952fe7337942534
Piyal <tenamax@gmail.com>
2019-04-09 12:36:41 +0530

    updates

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Webpack 4 Bootstrap
+
+> Webpack 4 with Babel, Bootstrap 4, jQuery and SCSS on board
+ 
+
+## Setup
+
+Install dependencies
+
+```sh
+$ npm i
+```
+
+## Scripts
+
+#### Run the local webpack-dev-server with livereload and autocompile on [http://localhost:8080/](http://localhost:8080/)
+
+```sh
+$ npm run dev
+```
+
+#### Build the current application for deployment
+
+```sh
+$ npm run build
+```
+
+#### Bootstrap code modification
+
+```sh
+change the bootstrap css @import "../../../src/scss/variables";
+change dependencies bootstrap change not to update
+```
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const DEV_MODE = process.env.NODE_ENV === 'dev'
+
+module.exports = {
+  devtool: DEV_MODE ? 'eval' : 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      DEV_MODE
+        ? {
+            test: /\.scss/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          }
+        : {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: ['css-loader', 'sass-loader']
+            })
+          },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new HtmlWebPackPlugin({
+      template: 'index.html'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ]
+}
